516a55ba346fefef8c53f3d4b48b5acb9f9504f4
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-15 13:10:46 +0100

    Adds spec for database table

--- spec/models/performance_data_spec.rb
+++ spec/models/performance_data_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PerformanceData, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Database table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :data }
+  end
+
+  describe 'Relations' do
+    it { is_expected.to belong_to :user }
+  end
 end
