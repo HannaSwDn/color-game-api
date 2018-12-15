615ccdbd3e6289a26d84983235be2e822910f22c
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-15 13:21:59 +0100

    Adds methods to performance_data controller: create, index and performance_data_params, tests that PerformanceDataController returns data

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe Api::V1::PerformanceDataController, type: :request do
-  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+  let(:user) { FactoryBot.create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
 
   describe 'POST /api/v1/performance_data' do
     it 'creates a data entry' do
@@ -9,6 +11,17 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
 
       entry = PerformanceData.last
       expect(entry.data).to eq 'message' => 'Average'
+    end
+  end
+
+  describe 'GET /api/v1/performance_data' do
+    before do
+      5.times { user.performance_data.create(data: { message: 'Average' }) }
+    end
+  
+    it 'returns a collection of performance data' do
+      get '/api/v1/performance_data', headers: headers
+      expect(response_json['entries'].count).to eq 5
     end
   end
 end
